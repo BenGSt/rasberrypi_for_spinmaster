2fd858efc7455a10aa865fd78656e3813f04f149
Yoav Silberstein <yoavz1@cs.huji.ac.il>
2021-11-22 22:15:56 +0200

    LCD control, draft, still in work...

--- spinmaster_lcd/lcd_ctrl.py
+++ spinmaster_lcd/lcd_ctrl.py
@@ -5,7 +5,7 @@
 Final project - LCD display shell interface
 Authors:    Yoav Silberstein
 File:	    lcd_ctrl.py
-Date:       19/11/2021  	
+Date:       22/11/2021  	
 ------------------------------------------------------
 '''
 from colorama.ansi import clear_line
@@ -37,25 +37,30 @@ def LCD_plot_line(text='', line=1, delay=0):
 
 
 # this function get a text and line and print the text on the LCD display
-def LCD_plot(text_arr):
-    text_arr_len = len(text_arr)
-    print(text_arr_len)
+def LCD_plot(text_lst, delay):
+    text_lst_len = len(text_lst)
     lcd_clear()                     # Clear the display of any data
     sleep(1)                        # delay of 1 second
 
-    if(text_arr_len == 1):    
-        LCD_plot_line(text_arr[0], 1)       # Write line of text to 1 line of display                
-    elif(text_arr_len == 2):    
-        LCD_plot_line(text_arr[0], 1)       # Write line of text to 1 line of display 
-        LCD_plot_line(text_arr[1], 2)       # Write line of text to 2 line of display   
-    else:
-        for i in range(text_arr_len * 2):
-            LCD_plot_line(text_arr[i % text_arr_len], 1)
-            LCD_plot_line(text_arr[(i + 1) % text_arr_len], 2)
-            sleep(1)                         # delay of 1 second
-            lcd_clear()
+  #  if(text_lst_len == 1):    
+   #     LCD_plot_line(text_lst[0], 1)       # Write line of text to 1 line of display                
+   # elif(text_lst_len == 2):    
+    #    LCD_plot_line(text_lst[0], 1)       # Write line of text to 1 line of display 
+     #   LCD_plot_line(text_lst[1], 2)       # Write line of text to 2 line of display   
     
-    return lst
+    for i in range(text_lst_len - 1):
+        lcd_clear()
+        
+        LCD_plot_line(text_lst[i % text_lst_len], 1)
+        LCD_plot_line(text_lst[(i + 1) % text_lst_len], 2)
+        sleep(1)                        # delay of 1 second
+
+    if(delay - 1 > 0):
+        sleep(delay - 1)   
+        lcd_clear()
+
+
+    
 
 def lcd_clear():
     display.lcd_clear() 
@@ -99,24 +104,31 @@ def help():
     print(Fore.YELLOW + "--print " + Fore.WHITE + "\"text\" print \"text\" on LCD display")
     print(Fore.YELLOW + "--get_ip " + Fore.WHITE + "print Raspberry pi ip on LCD display")
 
-
-def write_line_to_file(file_address, line_num, lst):
+def read_lines(file_address):
     #open and read the file after the appending:
     file = open(file_address, "r")
     r = file.read()
     lst = r.split('\n')
     file.close()
-    
-    lst[line_num - 1] = args.plot
+    return lst[:]
+
+def write_lines(file_address, lst):
+    #open and read the file after the appending:
     file = open(file_address, "w")
     file_lines = "\n".join(lst)
     file.write(file_lines)
-    print(lst)
-    print(type(lst))
     file.close()
+
+
+def write_line_to_file(file_address, line_num):
+    lst = read_lines(file_address)  
+    lst[line_num - 1] = args.plot
+    write_lines(file_address, lst)
     return lst[:]
 
 if __name__ == "__main__":
+
+    file_address = "memory"
     parser = argparse.ArgumentParser(description='LCD display interface')
     parser.add_argument('-p', '--plot', type=str, default='', help='print text on LCD screen --plot \"text\"')
     parser.add_argument('-l', '--line', type=int, default=1, help='line number')
@@ -125,8 +137,11 @@ if __name__ == "__main__":
 
 
     if(args.plot != ''):
-        lst = list()
-        new_lst = write_line_to_file('memory', args.line, lst)
-        LCD_plot(new_lst)
+        lst = write_line_to_file(file_address, args.line)
+        
+    else:  # print file to LCD
+        lst = read_lines(file_address)  
+
+    LCD_plot(lst, args.time) 
     
          
