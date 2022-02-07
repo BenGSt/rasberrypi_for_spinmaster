b428dbfd8c3ccb24cee54ff68725272f2fff3053
Ben <ben.g.steinberg@g,mai>
2022-02-07 22:08:56 +0200

    working on pwm

--- thermoelectric_cooler_control/test_TEC_duty-cycle_vs_vs_temp.sh
+++ thermoelectric_cooler_control/test_TEC_duty-cycle_vs_vs_temp.sh
@@ -14,8 +14,8 @@ main()
     ./hardware_pwm_via_sysfs.sh --channel 0 --operation ENABLE --frequency $FREQUENCY --duty-cycle $DUTY_CYCLE
     sleep 5s   # 5m
 
-    TEMP=`python /home/pi/raspberrypi_for_SpinMaster/sensors/thermistor_adc/read_thermistors.py | cut -f 2 | awk -F"=" '{print $2}'`
-    ROOM_TEMP=`python /home/pi/raspberrypi_for_SpinMaster/sensors/thermistor_adc/read_thermistors.py | cut -f 3 | awk -F"=" '{print $2}'`
+    TEMP=`sudo -u pi python /home/pi/raspberrypi_for_SpinMaster/sensors/thermistor_adc/read_thermistors.py | cut -f 2 | awk -F"=" '{print $2}'`
+    ROOM_TEMP=`sudo -u pi python /home/pi/raspberrypi_for_SpinMaster/sensors/thermistor_adc/read_thermistors.py | cut -f 3 | awk -F"=" '{print $2}'`
 
     printf $FREQUENCY"\t"$DUTY_CYCLE"\t"$TEMP"\t"$ROOM_TEMP"\n" | tee -a $OUT_FILE
   done
