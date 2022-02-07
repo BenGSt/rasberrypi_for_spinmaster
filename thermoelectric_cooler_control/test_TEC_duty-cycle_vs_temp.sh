78d275234793e00bd1125589d039a722ea8fa37e
Ben <ben.g.steinberg@g,mai>
2022-02-07 23:06:28 +0200

    working on pwm

--- thermoelectric_cooler_control/test_TEC_duty-cycle_vs_temp.sh
+++ thermoelectric_cooler_control/test_TEC_duty-cycle_vs_temp.sh
@@ -14,8 +14,13 @@ main()
     ./hardware_pwm_via_sysfs.sh --channel 0 --operation ENABLE --frequency $FREQUENCY --duty-cycle $DUTY_CYCLE
     sleep 5m
 
-    TEMP=`sudo -u pi python /home/pi/raspberrypi_for_SpinMaster/sensors/thermistor_adc/read_thermistors.py | cut -f 2 | awk -F"=" '{print $2}'`
-    ROOM_TEMP=`sudo -u pi python /home/pi/raspberrypi_for_SpinMaster/sensors/thermistor_adc/read_thermistors.py | cut -f 3 | awk -F"=" '{print $2}'`
+    #using the python script messed up readings
+#    TEMP=`sudo -u pi python /home/pi/raspberrypi_for_SpinMaster/sensors/thermistor_adc/read_thermistors.py | cut -f 2 | awk -F"=" '{print $2}'`
+#    ROOM_TEMP=`sudo -u pi python /home/pi/raspberrypi_for_SpinMaster/sensors/thermistor_adc/read_thermistors.py | cut -f 3 | awk -F"=" '{print $2}'`
+
+    #get readings from DB (mean of last 1m)
+    TEMP=`sudo influx -execute "SELECT mean("Tc_thermistor1") FROM "exe_thermistors_logfmt" WHERE time >= now() - 1m and time <= now() GROUP BY time(1m) fill(null)" -database="home" |awk 'NR==4 {printf("%.1f", $2)}'`
+    ROOM_TEMP=`sudo influx -execute "SELECT mean("Tc_thermistor2") FROM "exe_thermistors_logfmt" WHERE time >= now() - 1m and time <= now() GROUP BY time(1m) fill(null)" -database="home" |awk 'NR==4 {printf("%.1f", $2)}'`
 
     printf $FREQUENCY"\t"$DUTY_CYCLE"\t"$TEMP"\t"$ROOM_TEMP"\n" | tee -a $OUT_FILE
   done
