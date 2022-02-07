0d1ea2c63714c92ca6187c479671b4915d361e39
Ben <ben.g.steinberg@g,mai>
2022-02-07 21:40:48 +0200

    working on pwm

---
+++ thermoelectric_cooler_control/test_TEC_duty-cycle_vs_vs_temp.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+main()
+{
+  OUT_FILE = results.csv
+  FREQUENCY=20000
+
+  echo Frequency"\t"Duty_Cycle"\t"Temp"\t"Room_Temp | tee $OUT_FILE# header
+
+
+  for DUTY_CYCLE in `seq 5 5 100`
+  do
+    ./hardware_pwm_via_sysfs.sh --channel 0 --operation ENABLE --frequency $FREQUENCY --duty-cycle $DUTY_CYCLE
+    sleep 5m
+
+    TEMP=`python ../sensors/thermistor_adc/read_thermistors.py | cut -f 2 | awk -F"=" '{print $2}'`
+    ROOM_TEMP=`python ../sensors/thermistor_adc/read_thermistors.py | cut -f 3 | awk -F"=" '{print $2}'`
+
+    echo $FREQUENCY"\t"$DUTY_CYCLE"\t$"$TEMP"\t"$ROOM_TEMP | tee $OUT_FILE
+
+}}
+
+main
