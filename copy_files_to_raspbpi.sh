ba9eb95c118b88ca0a749aa5d0f20ea1c8ba44b0
Ben <ben.g.steinberg@g,mai>
2022-02-16 10:28:38 +0200

    working on control panel

--- copy_files_to_raspbpi.sh
+++ copy_files_to_raspbpi.sh
@@ -1,2 +1,11 @@
 # cgi backend scripts
 sudo cp ./dashboard/cgi-bin/* /usr/local/apache2/cgi-bin/
+
+#control panel web page
+sudo cp ./dashboard/webpage_with_embedded_grafana_dashboard.html /var/www/html/index.html
+
+# edit  /etc/apache2/sites-available/000-default.conf
+sudo cp ./dashboard/apache_000-default.conf  /etc/apache2/sites-available/000-default.conf
+
+#resart apache
+sudo apachectl -k graceful
