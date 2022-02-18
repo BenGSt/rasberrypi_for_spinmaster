e6dfa5e4e2239fb749bb0866d52209351364f554
Ben <ben.g.steinberg@g,mai>
2022-02-18 15:23:14 +0200

    working on control panel

--- spinmaster_main.sh
+++ spinmaster_main.sh
@@ -9,10 +9,18 @@ main()
 
   #start TEC cooling
   #sart TEC heating
+
   #sart pump
+    dma_pwm.sh --frequency $PUMP_PWM_FREQUENCY --duty-cycle $PUMP_PWM_DUTYCYCLE --gpio $PUMP_PWM_GPIO
   if [[use laser]]
     #start laser
 
-  #issue report
+  while [[current time < end_time]]
+    sleep 5m
+
+  #stop all PWMs
+  sudo killall pigpio
+
+  # issue report
   #save the run's data
 }
