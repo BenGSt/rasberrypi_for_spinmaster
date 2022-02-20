86ffd1ccac075705a13f94b67409d92b712d240d
Ben <ben.g.steinberg@g,mai>
2022-02-20 15:01:13 +0200

    working on PID

--- thermoelectric_cooler_control/PID.sh
+++ thermoelectric_cooler_control/PID.sh
@@ -31,17 +31,17 @@ main()
   previous_error=0
   integral=0
 
-#  while [[ 1 ]]
-#    do
-      measured_temp=`influx -execute "SELECT mean(\"Tc_thermistor$THERMISTOR_NUM\") FROM \"exe_thermistors_logfmt\" WHERE time >= now() - $AVG_TIME and time <= now() GROUP BY time(1m) fill(null)" -database="home" |awk 'NR==4 {printf("%.1f", $2)}'`
-      error=$(( setpoint - measured_temp ))
-      proportional=error
-      integral=$(( (integral + error) * dt ))
-      derivative=$(( (error - previous_error) / dt ))
-      output=$(( Kp * proportional + Ki * integral + Kd * derivative))
-      previous_error=error
-  #    sleep dt
-#    done
+  while [[ 1 ]]
+    do
+      echo measured_temp=`influx -execute "SELECT mean(\"Tc_thermistor$THERMISTOR_NUM\") FROM \"exe_thermistors_logfmt\" WHERE time >= now() - $AVG_TIME and time <= now() GROUP BY time(1m) fill(null)" -database="home" |awk 'NR==4 {printf("%.1f", $2)}'`
+      echo error=$(( setpoint - measured_temp ))
+      echo proportional=error
+      echo integral=$(( (integral + error) * dt ))
+      echo derivative=$(( (error - previous_error) / dt ))
+      echo output=$(( Kp * proportional + Ki * integral + Kd * derivative))
+      echo previous_error=error
+      sleep dt
+    done
 }
 
 
