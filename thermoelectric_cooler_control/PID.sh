012696d7a957d3b85d7761714bee2ce7eb8cb62c
Ben <ben.g.steinberg@g,mai>
2022-02-20 15:18:53 +0200

    working on PID

--- thermoelectric_cooler_control/PID.sh
+++ thermoelectric_cooler_control/PID.sh
@@ -35,7 +35,7 @@ main()
     do
       measured_temp=`influx -execute "SELECT mean(\"Tc_thermistor$THERMISTOR_NUM\") FROM \"exe_thermistors_logfmt\" WHERE time >= now() - $AVG_TIME and time <= now() GROUP BY time(1m) fill(null)" -database="home" |awk 'NR==4 {printf("%.1f", $2)}'`
       echo measured_temp=`influx -execute "SELECT mean(\"Tc_thermistor$THERMISTOR_NUM\") FROM \"exe_thermistors_logfmt\" WHERE time >= now() - $AVG_TIME and time <= now() GROUP BY time(1m) fill(null)" -database="home" |awk 'NR==4 {printf("%.1f", $2)}'`
-      error=$(bc -l <<< "$setpoint - $measured_temp" )
+      error=$(bc -l <<< "$measured_temp - $setpoint" )
       echo error=$(bc -l <<< "$setpoint - $measured_temp" )
       proportional=$error
       echo proportional=$error
