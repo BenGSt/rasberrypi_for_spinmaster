b93da594efe7f9fd40efbef32b69556c00a63015
Ben <ben.g.steinberg@g,mai>
2022-02-20 15:12:53 +0200

    working on PID

--- thermoelectric_cooler_control/PID.sh
+++ thermoelectric_cooler_control/PID.sh
@@ -35,16 +35,16 @@ main()
     do
       measured_temp=`influx -execute "SELECT mean(\"Tc_thermistor$THERMISTOR_NUM\") FROM \"exe_thermistors_logfmt\" WHERE time >= now() - $AVG_TIME and time <= now() GROUP BY time(1m) fill(null)" -database="home" |awk 'NR==4 {printf("%.1f", $2)}'`
       echo measured_temp=`influx -execute "SELECT mean(\"Tc_thermistor$THERMISTOR_NUM\") FROM \"exe_thermistors_logfmt\" WHERE time >= now() - $AVG_TIME and time <= now() GROUP BY time(1m) fill(null)" -database="home" |awk 'NR==4 {printf("%.1f", $2)}'`
-      error=$(( setpoint - measured_temp ))
-      echo error=$(( setpoint - measured_temp ))
-      proportional=error
-      echo proportional=error
-      integral=$(( (integral + error) * dt ))
-      echo integral=$(( (integral + error) * dt ))
-      derivative=$(( (error - previous_error) / dt ))
-      echo derivative=$(( (error - previous_error) / dt ))
-      output=$(( Kp * proportional + Ki * integral + Kd * derivative))
-      echo output=$(( Kp * proportional + Ki * integral + Kd * derivative))
+      error=$(bc - l <<< "$setpoint - $measured_temp" )
+      echo error=$(bc - l <<< "$setpoint - $measured_temp" )
+      proportional=$error
+      echo proportional=$error
+      integral=$(bc - l <<< "($integral + $error) * $dt" )
+      echo integral=$(bc - l <<< "($integral + $error) * $dt" )
+      derivative=$( bc - l <<< "($error - $previous_error) / $dt" )
+      echo derivative=$( bc - l <<< "($error - $previous_error) / $dt" )
+      output=$(bc - l <<< "$Kp * $proportional + $Ki * $integral + $Kd * $derivative")
+       output=$(bc - l <<< "$Kp * $proportional + $Ki * $integral + $Kd * $derivative")
       previous_error=$error
       echo previous_error=$error
 
