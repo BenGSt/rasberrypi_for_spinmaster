e016ec67528376b7c236c8455b78562608a44c6b
Ben <ben.g.steinberg@g,mai>
2022-02-20 14:47:53 +0200

    working on PID

--- thermoelectric_cooler_control/PID.sh
+++ thermoelectric_cooler_control/PID.sh
@@ -31,7 +31,7 @@ main()
   previous_error=0
   integral=0
 
-  while 1
+  while [[ 1 ]]
     do
       measured_temp=`influx -execute "SELECT mean(\"Tc_thermistor$THERMISTOR_NUM\") FROM \"exe_thermistors_logfmt\" WHERE time >= now() - $AVG_TIME and time <= now() GROUP BY time(1m) fill(null)" -database="home" |awk 'NR==4 {printf("%.1f", $2)}'`
       error=$(( setpoint − measured_value ))
