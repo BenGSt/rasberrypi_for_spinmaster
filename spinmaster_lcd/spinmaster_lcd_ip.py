7949ca6c7a634e4d614faa438bcf90f322b75146
Ben <ben.g.steinberg@g,mai>
2022-03-02 00:47:43 +0200

    working on polarimeter

--- spinmaster_lcd/spinmaster_lcd_ip.py
+++ spinmaster_lcd/spinmaster_lcd_ip.py
@@ -54,6 +54,7 @@ try:
         display.lcd_display_string("    Running    ", 2)
     else:
         display.lcd_display_string("    Standby    ", 2)
+        sleep(2)
         long_string(display, "Please see web based control panel at the displayed IP", 2)
 
     sleep(2)
