501a70f10c48c3ca42f99baa9f2453a96fbf6276
Ben <ben.g.steinberg@g,mai>
2022-03-05 20:33:21 +0200

    working on spinmaster_main.sh

--- dashboard/cgi-bin/stop_spinmaster_service.pl
+++ dashboard/cgi-bin/stop_spinmaster_service.pl
@@ -1,15 +1,8 @@
 #!/usr/bin/perl
-#$url = "/index.html";
-#print "Location: $url\n\n";
-#exec("sudo systemctl stop telegraf_spinmaster.service");
 
-print "Content-type: text/html\n\n";
-print "<HTML><HEAD>\n";
-print "<TITLE>CGI Test</TITLE>\n";
-print "</HEAD>\n";
-print "<BODY><A HREF=\"http://someplace.com\">Click Here</A>\n";
-print "</BODY></HTML>";
-
+$url = "/index.html";
+print "Location: $url\n\n";
+exec("sudo systemctl stop telegraf_spinmaster.service");
 exit;
 
 
