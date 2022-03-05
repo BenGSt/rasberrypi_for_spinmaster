2de685093c137897a73d59f7c4364cb400035162
Ben <ben.g.steinberg@g,mai>
2022-03-05 17:26:37 +0200

    working on spinmaster_main.sh

--- dashboard/cgi-bin/start_spinmaster_service.pl
+++ dashboard/cgi-bin/start_spinmaster_service.pl
@@ -9,6 +9,7 @@ my $flow_rate=$cgi->param('flow_rate');
 my $run_time=$cgi->param('run_time');
 
 $url = "/index.html";
+print CGI::header();
 print "Location: $url\n\n";
 #TODO: write spinmaster_main.service and pass params etc.
 #exec("sudo spinmaster_main.sh --fm_target_temperature $fm_target_temperature --reservoir_target_temperature $reservoir_target_temperature --flow_rate $flow_rate --run_time $run_time");
