78251bb6fe533277df2e4c8f9598a6d884f709c0
Ben <ben.g.steinberg@g,mai>
2022-03-09 17:02:50 +0200

    working on spinmaster_main.sh

--- dashboard/cgi-bin/start_spinmaster_service.pl
+++ dashboard/cgi-bin/start_spinmaster_service.pl
@@ -22,12 +22,12 @@ my $run_time=$cgi->param('run_time');
 #print FH $str;
 #close(FH);
 
-system("echo fm_target_temperature=$fm_target_temperature > /tmp/spin_master_service_environment_file")
-#system("printf ""fm_target_temperature="$fm_target_temperature", \nreservoir_target_temperature="$reservoir_target_temperature", \nflow_rate="$flow_rate", \nrun_time="$run_time" > /tmp/spin_master_service_environment_file2");
 
 $url = "/index.html";
 print "Location: $url\n\n";
 
+system("echo fm_target_temperature=$fm_target_temperature > /tmp/spin_master_service_environment_file")
+#system("printf ""fm_target_temperature="$fm_target_temperature", \nreservoir_target_temperature="$reservoir_target_temperature", \nflow_rate="$flow_rate", \nrun_time="$run_time" > /tmp/spin_master_service_environment_file2");
 #TODO: write spinmaster_main.service and pass params etc.
 #exec("sudo spinmaster_main.sh --fm_target_temperature $fm_target_temperature --reservoir_target_temperature $reservoir_target_temperature --flow_rate $flow_rate --run_time $run_time");
 exec("sudo systemctl start telegraf_spinmaster.service");
