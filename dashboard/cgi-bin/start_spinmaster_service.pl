19a15679f7995f8507448f7ad25668b01e9a6ea1
Ben <ben.g.steinberg@g,mai>
2022-03-09 17:08:07 +0200

    working on spinmaster_main.sh

--- dashboard/cgi-bin/start_spinmaster_service.pl
+++ dashboard/cgi-bin/start_spinmaster_service.pl
@@ -26,11 +26,11 @@ my $run_time=$cgi->param('run_time');
 my $url = "/index.html";
 print "Location: $url\n\n";
 
-system("echo fm_target_temperature="$fm_target_temperature" > /tmp/spin_master_service_environment_file");
+system("echo fm_target_temperature=$fm_target_temperature > /tmp/spin_master_service_environment_file");
 #system("printf ""fm_target_temperature="$fm_target_temperature", \nreservoir_target_temperature="$reservoir_target_temperature", \nflow_rate="$flow_rate", \nrun_time="$run_time" > /tmp/spin_master_service_environment_file2");
 #TODO: write spinmaster_main.service and pass params etc.
 #exec("sudo spinmaster_main.sh --fm_target_temperature $fm_target_temperature --reservoir_target_temperature $reservoir_target_temperature --flow_rate $flow_rate --run_time $run_time");
 exec("sudo systemctl start telegraf_spinmaster.service");
-#exec(`sudo bash -c "echo fm_target_temperature: $fm_target_temperature  > /home/pi/cgi_test.txt"`);
+exec(`sudo bash -c "echo fm_target_temperature: $fm_target_temperature  > /home/pi/cgi_test.txt"`);
 exit;
 
