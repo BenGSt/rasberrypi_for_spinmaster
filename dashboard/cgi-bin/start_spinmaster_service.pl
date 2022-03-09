d54b6f590d755b074737a812b292f78bfb61be5a
Ben <ben.g.steinberg@g,mai>
2022-03-09 17:59:50 +0200

    working on spinmaster_main.sh

--- dashboard/cgi-bin/start_spinmaster_service.pl
+++ dashboard/cgi-bin/start_spinmaster_service.pl
@@ -19,7 +19,7 @@ my $run_time=$cgi->param('run_time');
 
 $url = "/index.html";
 print "Location: $url\n\n";
-exec(`sudo bash -c "echo fm_target_temperature $fm_target_temperature reservoir_target_temperature $reservoir_target_temperature flow_rate $flow_rate run_time $run_time  > /home/pi/cgi_args"`);
+exec(`sudo bash -c "echo fm_target_temperature $fm_target_temperature reservoir_target_temperature $reservoir_target_temperature flow_rate $flow_rate run_time $run_time  > /tmp/spinmaster_service_env_file"`);
 #TODO: write spinmaster_main.service and pass params etc.
 #exec("sudo spinmaster_main.sh --fm_target_temperature $fm_target_temperature --reservoir_target_temperature $reservoir_target_temperature --flow_rate $flow_rate --run_time $run_time");
 #exec("sudo systemctl start telegraf_spinmaster.service");
