c5341e87a09c3dd671562dbdecb18c8a02da0dbc
Ben <ben.g.steinberg@g,mai>
2022-03-09 15:46:09 +0200

    working on spinmaster_main.sh

--- dashboard/cgi-bin/start_spinmaster_service.pl
+++ dashboard/cgi-bin/start_spinmaster_service.pl
@@ -8,6 +8,19 @@ my $reservoir_target_temperature=$cgi->param('reservoir_target_temperature');
 my $flow_rate=$cgi->param('flow_rate');
 my $run_time=$cgi->param('run_time');
 
+#write parameters to file for the service to use
+my $str = <<END;
+fm_target_temperature=$fm_target_temperature
+reservoir_target_temperature=$reservoir_target_temperature
+flow_rate=$flow_rate
+run_time=$run_time
+END
+
+my $filename = '/tmp/spin_master_service_environment_file';
+open(FH, '>', $filename) or die $!;
+print FH $str;
+close(FH);
+
 $url = "/index.html";
 print "Location: $url\n\n";
 
