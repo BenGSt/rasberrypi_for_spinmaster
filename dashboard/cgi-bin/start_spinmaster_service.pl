e1a61a7edd3225a3e8ff8a3b918c29d7dab04247
Ben <ben.g.steinberg@g,mai>
2022-03-09 17:23:26 +0200

    working on spinmaster_main.sh

--- dashboard/cgi-bin/start_spinmaster_service.pl
+++ dashboard/cgi-bin/start_spinmaster_service.pl
@@ -10,27 +10,27 @@ my $flow_rate=$cgi->param('flow_rate');
 my $run_time=$cgi->param('run_time');
 
 #write parameters to file for the service to use
-#my $str = <<END;
-#fm_target_temperature=$fm_target_temperature
-#reservoir_target_temperature=$reservoir_target_temperature
-#flow_rate=$flow_rate
-#run_time=$run_time
-#END
-#
-#my $filename = '/tmp/spin_master_service_environment_file';
-#open(FH, '>', $filename) or die $! , "died";
-#print FH $str;
-#close(FH);
+my $str = <<END;
+fm_target_temperature=$fm_target_temperature
+reservoir_target_temperature=$reservoir_target_temperature
+flow_rate=$flow_rate
+run_time=$run_time
+END
 
+my $filename = '/tmp/spin_master_service_environment_file';
+open(FH, '>', $filename) or die $! , "died";
+print FH $str;
+close(FH);
 
-my $url = "/index.html";
+system("echo fm_target_temperature=$fm_target_temperature > /tmp/spin_master_service_environment_file")
+system("printf ""fm_target_temperature="$fm_target_temperature", \nreservoir_target_temperature="$reservoir_target_temperature", \nflow_rate="$flow_rate", \nrun_time="$run_time" > /tmp/spin_master_service_environment_file2");
+
+$url = "/index.html";
 print "Location: $url\n\n";
 
-#system("echo fm_target_temperature=$fm_target_temperature > /tmp/spin_master_service_environment_file");
-#system("printf ""fm_target_temperature="$fm_target_temperature", \nreservoir_target_temperature="$reservoir_target_temperature", \nflow_rate="$flow_rate", \nrun_time="$run_time" > /tmp/spin_master_service_environment_file2");
 #TODO: write spinmaster_main.service and pass params etc.
 #exec("sudo spinmaster_main.sh --fm_target_temperature $fm_target_temperature --reservoir_target_temperature $reservoir_target_temperature --flow_rate $flow_rate --run_time $run_time");
-#exec("sudo systemctl start telegraf_spinmaster.service");
-exec(`sudo bash -c "echo fm_target_temperature: $fm_target_temperature  > /home/pi/cgi_test.txt"`);
+exec("sudo systemctl start telegraf_spinmaster.service");
+#exec(`sudo bash -c "echo fm_target_temperature: $fm_target_temperature  > /home/pi/cgi_test.txt"`);
 exit;
 
