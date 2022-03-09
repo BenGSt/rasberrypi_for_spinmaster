f461d9acf8ffbdc5fe8771870819f5cb175364a1
Ben <ben.g.steinberg@g,mai>
2022-03-09 17:01:08 +0200

    working on spinmaster_main.sh

--- dashboard/cgi-bin/start_spinmaster_service.pl
+++ dashboard/cgi-bin/start_spinmaster_service.pl
@@ -10,24 +10,20 @@ my $flow_rate=$cgi->param('flow_rate');
 my $run_time=$cgi->param('run_time');
 
 #write parameters to file for the service to use
-my $str = <<END;
-fm_target_temperature=$fm_target_temperature
-reservoir_target_temperature=$reservoir_target_temperature
-flow_rate=$flow_rate
-run_time=$run_time
-END
+#my $str = <<END;
+#fm_target_temperature=$fm_target_temperature
+#reservoir_target_temperature=$reservoir_target_temperature
+#flow_rate=$flow_rate
+#run_time=$run_time
+#END
+#
+#my $filename = '/tmp/spin_master_service_environment_file';
+#open(FH, '>', $filename) or die $! , "died";
+#print FH $str;
+#close(FH);
 
-my $filename = '/tmp/spin_master_service_environment_file';
-open(FH, '>', $filename) or die $! , "died";
-print FH $str;
-close(FH);
-
-system("cat<<END > /tmp/spin_master_service_environment_file2
-reservoir_target_temperature=$reservoir_target_temperature
-flow_rate=$flow_rate
-run_time=$run_time
-END
-");
+system("echo fm_target_temperature=$fm_target_temperature > /tmp/spin_master_service_environment_file")
+#system("printf ""fm_target_temperature="$fm_target_temperature", \nreservoir_target_temperature="$reservoir_target_temperature", \nflow_rate="$flow_rate", \nrun_time="$run_time" > /tmp/spin_master_service_environment_file2");
 
 $url = "/index.html";
 print "Location: $url\n\n";
