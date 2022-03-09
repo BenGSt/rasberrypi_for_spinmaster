bb3b96046fdeef14fd45c010c24f44f989f792b2
Ben <ben.g.steinberg@g,mai>
2022-03-09 19:03:38 +0200

    working on spinmaster_main.sh

--- dashboard/cgi-bin/start_spinmaster_service.pl
+++ dashboard/cgi-bin/start_spinmaster_service.pl
@@ -35,7 +35,7 @@ exec(`sudo bash -c "echo  run_time=$run_time  >> /home/pi/spinmaster_service_env
 #TODO: write spinmaster_main.service and pass params etc.
 #exec("sudo spinmaster_main.sh --fm_target_temperature $fm_target_temperature --reservoir_target_temperature $reservoir_target_temperature --flow_rate $flow_rate --run_time $run_time");
 exec("sudo systemctl start telegraf_spinmaster.service");
-exec("sudo systemctl start spinmaster_main.service");
+system("sudo systemctl start spinmaster_main.service");
 #exec(`sudo bash -c "echo fm_target_temperature: $fm_target_temperature  > /home/pi/cgi_test.txt"`);
 exit;
 
