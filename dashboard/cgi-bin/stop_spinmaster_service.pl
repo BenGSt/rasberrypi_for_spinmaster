72171741c98da31d90bb1259e35e6d7ee4e05682
Ben <ben.g.steinberg@gmail.com>
2022-04-18 14:46:50 +0300

    shutdown debugging

--- dashboard/cgi-bin/stop_spinmaster_service.pl
+++ dashboard/cgi-bin/stop_spinmaster_service.pl
@@ -3,11 +3,11 @@
 $url = "/index.html";
 print "Location: $url\n\n";
 #exec("sudo systemctl stop telegraf_spinmaster.service");
-exec("sudo systemctl stop spinmaster_main.service");
-exec("sudo bash -c 'home/pi/raspberrypi_for_SpinMaster/run/shutdown_spinmaster_failsafe.sh > /home/pi/spinmaster_failsafe_last_run.log 2>&1'");
+system("sudo systemctl stop spinmaster_main.service");
+system("sudo bash -c 'home/pi/raspberrypi_for_SpinMaster/run/shutdown_spinmaster_failsafe.sh > /home/pi/spinmaster_failsafe_last_run.log 2>&1'");
 exit;
 
-
+#NOTE: https://stackoverflow.com/questions/799968/whats-the-difference-between-perls-backticks-system-and-exec
 
 
 
