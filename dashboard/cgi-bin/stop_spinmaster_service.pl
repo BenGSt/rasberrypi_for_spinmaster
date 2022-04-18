a1ad0fcfbec7446c7b8a56eb7cc25071a5d18e1b
Ben <ben.g.steinberg@gmail.com>
2022-04-18 14:35:23 +0300

    shutdown debugging

--- dashboard/cgi-bin/stop_spinmaster_service.pl
+++ dashboard/cgi-bin/stop_spinmaster_service.pl
@@ -4,7 +4,7 @@ $url = "/index.html";
 print "Location: $url\n\n";
 #exec("sudo systemctl stop telegraf_spinmaster.service");
 exec("sudo systemctl stop spinmaster_main.service");
-exec("sudo /home/pi/raspberrypi_for_SpinMaster/run/shutdown_spinmaster_failsafe.sh > /home/pi/spinmaster_failsafe_last_run.log 2>&1 ");
+exec("sudo bash -c 'home/pi/raspberrypi_for_SpinMaster/run/shutdown_spinmaster_failsafe.sh > /home/pi/spinmaster_failsafe_last_run.log 2>&1');
 exit;
 
 
